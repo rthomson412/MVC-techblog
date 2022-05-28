e3016fafa0554805872667cf35ccc0d47ff764ca
RossT <rthomson412@gmail.com>
2022-05-27 22:56:43 -0400

    utils

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+//redirects unauthenticated users attempting to access a page to the login page
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
